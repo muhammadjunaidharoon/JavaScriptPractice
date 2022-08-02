da33e8d1059434358ca2977fee91eb2ce6ede2dd
Muhammad Junaid Haroon <105660716+muhammadjunaidharoon@users.noreply.github.com>
2022-08-02 21:23:32 +0500

    Node Js Hello Word

---
+++ WeatherApp/scrip.js
@@ -0,0 +1,15 @@
+// function getWeather(){
+
+//     let cityName = document.getElementById("cityName").ariaValueMax;
+
+//     cityName.addEventListener("keyup", e =>{
+//         if(e.key == "Enter" && cityName.value != ""){
+//             alert ("key press")
+//             // requestApi(inputField.value);
+//         }
+//     });
+      
+// console.log (cityName);    
+
+    
+// }
